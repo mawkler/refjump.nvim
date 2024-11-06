558a74a1ec49848e91032a5699fd874f8034af04
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-06 14:37:25 +0700

    fix typo in keymap descriptions

--- lua/refjump/keymaps.lua
+++ lua/refjump/keymaps.lua
@@ -27,8 +27,8 @@ function M.create_keymaps(opts)
       and repeatable_jump_map
       or jump_map
 
-  vim.keymap.set(nxo, opts.keymaps.next, jump({ forward = true }), { desc = 'Referece jump forward' })
-  vim.keymap.set(nxo, opts.keymaps.prev, jump({ forward = false }), { desc = 'Referece jump backward' })
+  vim.keymap.set(nxo, opts.keymaps.next, jump({ forward = true }), { desc = 'Reference jump forward' })
+  vim.keymap.set(nxo, opts.keymaps.prev, jump({ forward = false }), { desc = 'Reference jump backward' })
 end
 
 return M
